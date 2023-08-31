ad60863118ddf56e99f1fb91f7d78c71a79880f0
vsabouroux <vsabouroux@aol.com>
2023-08-31 16:01:25 +0200

    Initial commit

---
+++ FrontEnd/js/projets.js
@@ -0,0 +1,13 @@
+[
+    {
+      "id": i,
+      "title": "",
+      "imageUrl": "http://localhost:5678/images/",
+      "categoryId": i,
+      "userId": i,
+      "category": {
+        "id": i,
+        "name": ""
+      }
+    }
+  ]
