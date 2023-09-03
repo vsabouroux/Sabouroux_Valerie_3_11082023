f8b07935bc26c0f82989f06902c7da1687f48f1a
vsabouroux <vsabouroux@aol.com>
2023-09-03 18:34:25 +0200

    Update index.js

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -5,13 +5,6 @@ fetch("http://localhost:5678/api/works")
     // Traitement des données récupérées ici
     console.log(projets);
     genererProjets(projets);
-
-    fetch("http://localhost:5678/api/categories")
-      .then((response) => response.json())
-      .then((categories) => {
-        console.log(projets);
-        filtrerProjetsParCategories(projets);
-      });
   })
   .catch((error) => {
     console.error(
@@ -19,51 +12,8 @@ fetch("http://localhost:5678/api/works")
       error
     );
   });
-
-// récupération des CATEGORIES sur le localhost du BackEnd
-//fetch("http://localhost:5678/api/categories")
-//  .then((response) => response.json())
-// .then((categories) => {
-//Fonction pour create les catégories et gestion du filtre
-
-// Traitement des données récupérées ici
-//1 - btn "tous" donc PAS DE TRI sur les projets
-// const boutonFiltreTous = document.querySelector(".btn-tous");
-// boutonFiltreTous.addEventListener("click", function () {
-//   // on vide la page pour la remettre à jour du filtre
-//   document.querySelector(".gallery").innerHTML = "";
-//   genererProjets(projets);
-// });
-// //2 filtrer les projets pour récupérer QUE les "objets"
-// const boutonObjets = document.querySelector(".btn-objets");
-
-// boutonObjets.addEventListener("click", function () {
-//   const projetsFiltres = projets.filter(function (projet) {
-//     return projet.category.name === "Objets";
-//   });
-//   console.log(projetsFiltres);
-//   //document.querySelector(".gallery").innerHTML ="";
-//   //genererProjets (projetsFiltres);
-// });
-//   console.log(categories);
-// })
-// .catch((error) => {
-//  console.error(
-//  "Une erreur s'est produite lors de la récupération des données :",
-//error
-//   );
-// });
-//création d'une fonction filtreCategories qui permet au clic de filtrer les projets en fonction du choix de l'utilisateur
-function filtrerProjetsParCategories(projets) {
-  const divFiltres = document.querySelector(".filtres");
-  for (const projet of projets) {
-    const categoryElement = document.createElement("p");
-    categoryElement.innerText = projet.category.name;
-
-    divFiltres.appendChild(categoryElement);
-  }
-  console.log(categories);
-}
+//Chaque projet "embarque" la CATEGORIE donc idée d'inserer dans la boucle for of l'écoute du clic
+//Gestion des boutons
 
 function genererProjets(projets) {
   const divGallery = document.querySelector(".gallery");
@@ -79,10 +29,21 @@ function genererProjets(projets) {
     imageElement.src = projet.imageUrl;
     const titleElement = document.createElement("h3");
     titleElement.innerText = projet.title;
+    const categoryElement = document.createElement("button");
+    categoryElement.innerText = projet.category.name;
 
     // liaisons pour affichage sur la page web
     divGallery.appendChild(projetElement);
     projetElement.appendChild(imageElement);
     projetElement.appendChild(titleElement);
+    divFiltres.appendChild(categoryElement);
   }
 }
+//creation des boutons
+const divFiltres = document.querySelector(".filtres");
+
+const boutonTous = document.querySelector(".filter_active");
+boutonTous.addEventListener("click", function () {
+  genererProjets(projets);
+});
+// fonction .filter pour les autres boutons
