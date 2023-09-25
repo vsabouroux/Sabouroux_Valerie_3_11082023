47450a69b7fd5eb16846d12605260a6125a80f51
vsabouroux <vsabouroux@aol.com>
2023-09-25 11:41:07 +0200

    travail pour ouvrir modal pas encore fonctionnel

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,22 +1,30 @@
-// Ajout de l'événement click sur le bouton "modifier" pour ouvrir la modale
+// Ajout de l'événement click sur le bouton "modifier"de la page accueil "loguée" pour ouvrir la modale
 // Récupérez les éléments DOM pertinents
-const btnModifier = document.getElementById("btnModifier");
+const btnModifier = document.querySelector(".modifier-button");
 const modal = document.getElementById("modal");
 const closeBtn = document.querySelector(".close");
 
-// Fonction pour ouvrir la modale
-btnModifier.addEventListener("click", () => {
+// Fonction pour ouvrir la modale lorsque le bouton est cliqué
+function ouvrirModal() {
   modal.style.display = "block";
-});
+}
 
 // Fonction pour fermer la modale
-closeBtn.addEventListener("click", () => {
+function fermerModal() {
   modal.style.display = "none";
-});
+}
 
-// Fermez la modale si l'utilisateur clique en dehors de celle-ci
+// Ajout gestionnaire d'événement click au bouton "Modifier"
+btnModifier.addEventListener("click", ouvrirModal);
+
+// Ajout gestionnaire d'événement click au bouton de fermeture (X)
+closeBtn.addEventListener("click", fermerModal);
+
+// Ferme ture de la modale si l'utilisateur clique en dehors de celle-ci
 window.addEventListener("click", (event) => {
   if (event.target === modal) {
-    modal.style.display = "none";
+    fermerModal();
   }
 });
+ouvrirModal();
+fermerModal();
